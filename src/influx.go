f29159b04201e182ea350841e5154b8394b6117a
Erikas Rudinskas <erikmnkl@gmail.com>
2019-10-06 19:59:15 +0300

    Fix incorrect SQL query

--- src/influx.go
+++ src/influx.go
@@ -15,15 +15,15 @@ func defineInfluxHTTP() {
 
 func printInfluxData(w *http.ResponseWriter) {
 	sql := `
-	SELECT "portal" AS "type", "alio.lt" AS "key", COUNT(*) AS "value" FROM posts WHERE url LIKE "%alio.lt%"
-	UNION SELECT "portal" AS "type", "aruodas.lt" AS "key", COUNT(*) AS "value" FROM posts WHERE url LIKE "%aruodas.lt%"
-	UNION SELECT "portal" AS "type", "domoplius.lt" AS "key", COUNT(*) AS "value" FROM posts WHERE url LIKE "%domoplius.lt%"
-	UNION SELECT "portal" AS "type", "kampas.lt" AS "key", COUNT(*) AS "value" FROM posts WHERE url LIKE "%kampas.lt%"
-	UNION SELECT "portal" AS "type", "nuomininkai.lt" AS "key", COUNT(*) AS "value" FROM posts WHERE url LIKE "%nuomininkai.lt%"
-	UNION SELECT "portal" AS "type", "rinka.lt" AS "key", COUNT(*) AS "value" FROM posts WHERE url LIKE "%rinka.lt%"
-	UNION SELECT "portal" AS "type", "skelbiu.lt" AS "key", COUNT(*) AS "value" FROM posts WHERE url LIKE "%skelbiu.lt%"
-	UNION SELECT "users" AS "type", "visited" AS "key", COUNT(*) AS "value" FROM users
-	UNION SELECT "users" AS "type", "enabled" AS "key", COUNT(*) AS "value" FROM users WHERE enabled = 1
+	SELECT 'portal' AS "type", 'alio.lt' AS "key", COUNT(*) AS "value" FROM posts WHERE url LIKE "%alio.lt%"
+	UNION SELECT 'portal' AS "type", 'aruodas.lt' AS "key", COUNT(*) AS "value" FROM posts WHERE url LIKE "%aruodas.lt%"
+	UNION SELECT 'portal' AS "type", 'domoplius.lt' AS "key", COUNT(*) AS "value" FROM posts WHERE url LIKE "%domoplius.lt%"
+	UNION SELECT 'portal' AS "type", 'kampas.lt' AS "key", COUNT(*) AS "value" FROM posts WHERE url LIKE "%kampas.lt%"
+	UNION SELECT 'portal' AS "type", 'nuomininkai.lt' AS "key", COUNT(*) AS "value" FROM posts WHERE url LIKE "%nuomininkai.lt%"
+	UNION SELECT 'portal' AS "type", 'rinka.lt' AS "key", COUNT(*) AS "value" FROM posts WHERE url LIKE "%rinka.lt%"
+	UNION SELECT 'portal' AS "type", 'skelbiu.lt' AS "key", COUNT(*) AS "value" FROM posts WHERE url LIKE "%skelbiu.lt%"
+	UNION SELECT 'users' AS "type", 'visited' AS "key", COUNT(*) AS "value" FROM users
+	UNION SELECT 'users' AS "type", 'enabled' AS "key", COUNT(*) AS "value" FROM users WHERE enabled = 1
 	`
 
 	rows, err := db.Query(sql)
