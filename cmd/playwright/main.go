0b8a6b533a94804c1e8985e6f030572a53a2780c
Erikas <erkexzcx@users.noreply.github.com>
2024-02-13 21:19:00 +0200

    fool around with playwright-go

---
+++ cmd/playwright/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/playwright-community/playwright-go"
+)
+
+func main() {
+	opts := playwright.BrowserTypeLaunchOptions{
+		ExecutablePath: playwright.String("/usr/bin/chromium"),
+		Headless:       playwright.Bool(false),
+	}
+
+	pw, err := playwright.Run()
+	if err != nil {
+		log.Fatalf("could not start playwright: %v", err)
+	}
+	browser, err := pw.Chromium.Launch(opts)
+	if err != nil {
+		log.Fatalf("could not launch browser: %v", err)
+	}
+	page, err := browser.NewPage()
+	if err != nil {
+		log.Fatalf("could not create page: %v", err)
+	}
+	if _, err = page.Goto("https://news.ycombinator.com"); err != nil {
+		log.Fatalf("could not goto: %v", err)
+	}
+	entries, err := page.Locator(".athing").All()
+	if err != nil {
+		log.Fatalf("could not get entries: %v", err)
+	}
+	for i, entry := range entries {
+		title, err := entry.Locator("td.title > span > a").TextContent()
+		if err != nil {
+			log.Fatalf("could not get text content: %v", err)
+		}
+		fmt.Printf("%d: %s\n", i+1, title)
+	}
+	if err = browser.Close(); err != nil {
+		log.Fatalf("could not close browser: %v", err)
+	}
+	if err = pw.Stop(); err != nil {
+		log.Fatalf("could not stop Playwright: %v", err)
+	}
+}
--- go.mod
+++ go.mod
@@ -11,8 +11,17 @@ require (
 )
 
 require (
+	github.com/deckarep/golang-set/v2 v2.6.0 // indirect
+	github.com/go-jose/go-jose/v3 v3.0.1 // indirect
+	github.com/go-stack/stack v1.8.1 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
+	golang.org/x/exp v0.0.0-20240103183307-be819d1f06fc // indirect
+)
+
+require (
 	github.com/andybalholm/cascadia v1.3.2 // indirect
 	github.com/google/uuid v1.6.0 // indirect
+	github.com/playwright-community/playwright-go v0.4101.1
 	github.com/robfig/cron/v3 v3.0.1 // indirect
 	go.uber.org/atomic v1.11.0 // indirect
 	golang.org/x/net v0.21.0 // indirect
