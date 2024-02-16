7ec94cf37eca496d9ead1bda72e092342249a054
Erikas <erkexzcx@users.noreply.github.com>
2024-02-16 20:27:58 +0200

    add poc for aruodas website

--- cmd/playwright/main.go
+++ cmd/playwright/main.go
@@ -1,43 +1,56 @@
 package main
 
+/*
+Proof of concept that it works with Aruodas
+*/
+
 import (
 	"fmt"
 	"log"
+	"strings"
 
 	"github.com/playwright-community/playwright-go"
 )
 
 func main() {
-	opts := playwright.BrowserTypeLaunchOptions{
+	launchOpts := playwright.BrowserTypeLaunchOptions{
 		ExecutablePath: playwright.String("/usr/bin/chromium"),
-		Headless:       playwright.Bool(false),
+		Headless:       playwright.Bool(true),
 	}
+	link := "https://m.aruodas.lt/?obj=4&FRegion=461&FDistrict=1&FOrder=AddDate&from_search=1&detailed_search=1&FShowOnly=FOwnerDbId0%2CFOwnerDbId1&act=search"
+	ua := "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
 
 	pw, err := playwright.Run()
 	if err != nil {
 		log.Fatalf("could not start playwright: %v", err)
 	}
-	browser, err := pw.Chromium.Launch(opts)
+	browser, err := pw.Chromium.Launch(launchOpts)
 	if err != nil {
 		log.Fatalf("could not launch browser: %v", err)
 	}
-	page, err := browser.NewPage()
+
+	context, _ := browser.NewContext(playwright.BrowserNewContextOptions{
+		UserAgent: playwright.String(ua),
+	})
+
+	page, err := context.NewPage()
 	if err != nil {
 		log.Fatalf("could not create page: %v", err)
 	}
-	if _, err = page.Goto("https://news.ycombinator.com"); err != nil {
+	if _, err = page.Goto(link); err != nil {
 		log.Fatalf("could not goto: %v", err)
 	}
-	entries, err := page.Locator(".athing").All()
+	entries, err := page.Locator(".item-address-v4").All()
 	if err != nil {
 		log.Fatalf("could not get entries: %v", err)
 	}
 	for i, entry := range entries {
-		title, err := entry.Locator("td.title > span > a").TextContent()
+		contents, err := entry.TextContent()
+		contents = strings.TrimSpace(contents)
 		if err != nil {
 			log.Fatalf("could not get text content: %v", err)
 		}
-		fmt.Printf("%d: %s\n", i+1, title)
+		fmt.Printf("%d: %s\n", i+1, contents)
 	}
 	if err = browser.Close(); err != nil {
 		log.Fatalf("could not close browser: %v", err)
