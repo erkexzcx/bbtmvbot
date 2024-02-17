92e5736e022b7f54a65d8285a60de0134aa28480
Erikas <erkexzcx@users.noreply.github.com>
2024-02-17 20:55:53 +0200

    nuomininkai fixed

--- website/all/all.go
+++ website/all/all.go
@@ -5,8 +5,8 @@ import (
 	//FIXED _ "bbtmvbot/website/alio"
 	//FIXED _ "bbtmvbot/website/aruodas"
 	//FIXED _ "bbtmvbot/website/domoplius"
-	_ "bbtmvbot/website/kampas"
-	// _ "bbtmvbot/website/nuomininkai"
+	//FIXED _ "bbtmvbot/website/kampas"
+	_ "bbtmvbot/website/nuomininkai"
 	// _ "bbtmvbot/website/rinka"
 	// _ "bbtmvbot/website/skelbiu"
 )
